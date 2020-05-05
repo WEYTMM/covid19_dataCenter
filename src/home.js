e9aa3bc86a235d90ba0832d6ac8076f5881775a8
Bro Sand <bsand8813@gmail.com>
2020-05-05 18:51:46 +0800

    penyesuaian tampilan

---
+++ src/home.js
@@ -0,0 +1,11 @@
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+      document.getElementById("navbar").style.backgroundColor = "#130947";
+      document.getElementById("navbar").style.transition = "0.3s";
+    } else {
+      document.getElementById("navbar").style.backgroundColor = "rgba(0, 0, 0, 0)";
+      document.getElementById("navbar").style.transition = "0.3s";
+    }
+  }
