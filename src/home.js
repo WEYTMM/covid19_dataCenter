bac96826ae090e3a8d60c8fdb24920542b61a5b9
Bro Sand <bsand8813@gmail.com>
2020-05-11 18:44:02 +0800

    menambahkan count

--- src/home.js
+++ src/home.js
@@ -1,7 +1,7 @@
 // Manipulate DOM HTML
 
 
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function() {scrollFunction(), numberFunction()};
 
 function scrollFunction() {
     if (document.body.scrollTop > 150 || document.documentElement.scrollTop > 150) {
@@ -41,22 +41,19 @@ function stopVideo(modal){
 
 // Scroll into view
 
-window.onload=function(){
-  berandaklik = document.getElementById("kilikberanda");
+
+  var berandaklik = document.getElementById("klikberanda");
   berandaklik.addEventListener("click", berandascroll);
 
-  statistikklik = document.getElementById("klikstatistika");
+  var statistikklik = document.getElementById("klikstatistika");
   statistikklik.addEventListener("click",statistikscroll);
-}
 
-// window.onload=function(){
-  
-// }
+  var covidklik = document.getElementById("klikcovid");
+  covidklik.addEventListener("click",covidscroll);
 
-// window.onload=function(){
-//   covidklik = document.getElementById("klikcovid");
-//   covidklik.addEventListener("click",covidscroll);
-// }
+
+  var tentangklik = document.getElementById("kliktentang");
+  tentangklik.addEventListener("click",tentangscroll);
 
 
 function berandascroll(){
@@ -78,11 +75,61 @@ function statistikscroll(){
   });
 }
 
-// function covidscroll(){
-//   var cv= document.getElementById("halamancovid");
-//   cv.scrollIntoView({
-//     behavior: "smooth",
-//     inline : "center",
-//     block : "nearest"
-//   });
-// }
+function covidscroll(){
+  var cv = document.getElementById("halamancovid");
+  cv.scrollIntoView({
+    behavior: "smooth",
+    inline : "center",
+    block : "nearest"
+  });
+}
+
+function tentangscroll(){
+  var tt = document.getElementById("halamantentang");
+  tt.scrollIntoView();
+  console.log("sudahklik");
+}
+
+
+function numberFunction(){
+  if(document.body.scrollTop > 250 || document.documentElement.scrollTop > 250){
+    animateValue("yellow", 0, 2000, 2000);
+    animateValue("green",0,200,2000);
+    animateValue("red",0,20,2000);
+  }
+}
+
+
+
+//
+function animateValue(id, start, end, duration) {
+  // assumes integer values for start and end
+  
+  var obj = document.getElementById(id);
+  var range = end - start;
+  // no timer shorter than 50ms (not really visible any way)
+  var minTimer = 50;
+  // calc step time to show all interediate values
+  var stepTime = Math.abs(Math.floor(duration / range));
+  
+  // never go below minTimer
+  stepTime = Math.max(stepTime, minTimer);
+  
+  // get current time and calculate desired end time
+  var startTime = new Date().getTime();
+  var endTime = startTime + duration;
+  var timer;
+
+  function run() {
+      var now = new Date().getTime();
+      var remaining = Math.max((endTime - now) / duration, 0);
+      var value = Math.round(end - (remaining * range));
+      obj.innerHTML = value;
+      if (value == end) {
+          clearInterval(timer);
+      }
+  }
+  
+  timer = setInterval(run, stepTime);
+  run();
+}
